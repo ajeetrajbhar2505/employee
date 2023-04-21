fe6ead808dbbf15bbe39ac6b40d018bcca37dab2
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-04-21 14:09:20 +0530

    first commit

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input ,SimpleChanges} from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -9,40 +9,41 @@ export class TableComponent {
 
   searchEmployee = ""
   @Input() employeeData: any[] = []
-  filteredEmployeeData:any[] =[]
+  filteredEmployeeData: any[] = []
   rangeData: any[] = []
 
- 
 
-//  data is fetched asynchronously from an API, child component is being rendered before the data is available,
-// To solve this, we can use the Angular ngOnChanges() 
+
+  //  data is fetched asynchronously from an API, child component is being rendered before the data is available,
+  // To solve this, we can use the Angular ngOnChanges() 
   ngOnChanges(changes: SimpleChanges) {
     if (changes) {
       // Handle changes to the employeeData array
-    this.filteredEmployeeData = [...this.employeeData].sort((a:any,b:any) => a.id - b.id)
-    const oldestEmployee = this.filteredEmployeeData.sort((a:any,b:any)=> b.employee_age - a.employee_age)[0]
-    const oldestAge = oldestEmployee.employee_age;
-    const agerangeDifference = 20
-    const agerangeSize = Math.ceil(oldestAge / agerangeDifference);
-    
-    for (let i = 0; i < agerangeSize; i++) {
-      let ageRangeStart = i * agerangeDifference + 1
-      let ageRangeEnd = (i + 1) * agerangeDifference
-      this.rangeData.push(`${ageRangeStart}-${ageRangeEnd}`)
+      this.filteredEmployeeData = [...this.employeeData].sort((a: any, b: any) => a.id - b.id)
+
+      const oldestEmployee = this.filteredEmployeeData.sort((a: any, b: any) => b.employee_age - a.employee_age)[0]
+      const oldestAge = oldestEmployee.employee_age;
+      const agerangeDifference = 20
+      const agerangeSize = Math.ceil(oldestAge / agerangeDifference);
+
+      for (let i = 0; i < agerangeSize; i++) {
+        let ageRangeStart = i * agerangeDifference + 1
+        let ageRangeEnd = (i + 1) * agerangeDifference
+        this.rangeData.push(`${ageRangeStart}-${ageRangeEnd}`)
+      }
     }
   }
-  }
-  
+
 
   filterOrderBy(event: any) {
     let sortOrder = event.target.value
     this.filteredEmployeeData = this.employeeData.sort((a: any, b: any) =>
-    // ternary comparison
+      // ternary comparison
       sortOrder === 'asc' ? a.employee_salary - b.employee_salary : b.employee_salary - a.employee_salary
     );
   }
 
-  
+
   filterRangeBy(event: any) {
     let range = event.target.value;
     const [start, end] = range.split('-').map(Number);
@@ -50,6 +51,6 @@ export class TableComponent {
       employee.employee_age >= start && employee.employee_age <= end
     );
   }
-  
+
 
 }
